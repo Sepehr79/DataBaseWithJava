d13d369e31e8a4968c13944f5113421cc144935b
Sepehr <sepehrmsm1379@gmail.com>
2020-09-14 13:21:02 +0430

    commited to git

---
+++ main/MysqlTutorials/MainClass.java
@@ -0,0 +1,14 @@
+package DataBaseWithJava.main.MysqlTutorials;
+
+
+import DataBaseWithJava.main.MysqlTutorials.DataBase.MysqlDataBase;
+
+
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        System.out.println(MysqlDataBase.getStudents());
+
+    }
+}
